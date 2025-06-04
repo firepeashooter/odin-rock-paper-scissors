73e2b145afbc03af424a799235cdd78b28e1e10c
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-04 19:00:50 -0400

    Add a function to play a single round

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 
 
+let humanScore = 0
+let computerScore = 0
+
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
 }
@@ -24,5 +27,41 @@ function getHumanChoice(){
 }
 
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice){
 
+    humanChoice = humanChoice.toLowerCase()
+    computerChoice = computerChoice.toLowerCase()
+
+    if (humanChoice == computerChoice){
+        console.log("Draw! You both picked" + humanChoice)
+    }else if (humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors")
+        humanScore += 1        
+    }else if (humanChoice == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock!")
+        computerScore += 1
+    }else if (humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock!")
+        humanScore += 1
+    }else if (humanChoice == "paper" && computerChoice == 'scissors'){
+        console.log("You lose! Scissors beats Paper!")
+        computerScore += 1
+    }else if (humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors!")
+        computerScore += 1
+    }else if (humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You won! Scissors beats Paper!")
+        humanScore += 1
+    }
+
+}
+
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice()
+
+
+console.log(humanChoice)
+console.log(computerChoice)
+
+
+playRound(humanChoice, computerChoice)
